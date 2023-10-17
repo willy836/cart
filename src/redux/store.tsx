8fbc4b06a03b56eb600b0f5183f739fd6e5f0ac5
willy836 <wilsonkimiri836@gmail.com>
2023-10-17 16:53:10 +0300

    Modal slice

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cart/cartSlice";
+import modalReducer from "./modal/modalSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    modal: modalReducer,
   },
 });
 
